bc2d9be7d47607d37aaf23f8586ef57f3ce921de
fayezdeve <161427745+fayezdeve@users.noreply.github.com>
2024-08-28 23:57:45 +0300

    Delete wp-content directory

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-1322.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-6d826c2c' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-3708bbe4' );
-});
- });
